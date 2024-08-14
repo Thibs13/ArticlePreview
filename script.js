e240b6714b71dcdb126055f7032a614d6c70bc80
Thibault <caumontthibault1313@gmail.com>
2024-08-14 18:24:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const user = document.querySelector(".user")
 const socialmedia = document.querySelector(".social-media")
 const footer = document.querySelector("footer")
 const btnshare2 = document.querySelector(".share2")
-
+const btnshare3 = document.querySelector(".share3")
+const socialmedia2 = document.querySelector(".socialmedia2")
 btnshare.addEventListener("click",()=>{
     user.classList.toggle("inv")
     btnshare.classList.toggle("inv")
@@ -13,4 +14,8 @@ btnshare2.addEventListener("click",()=>{
     user.classList.toggle("inv")
     btnshare.classList.toggle("inv")
     footer.classList.toggle("inv")
+})
+btnshare3.addEventListener("click",()=>{
+    socialmedia2.classList.toggle("inv")
+    btnshare3.classList.toggle("switchcolor")
 })
